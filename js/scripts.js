448249e3c8be23615ecb2fb144bbf787ab0f3e6c
Adam Gorbahn and Martin Cartledge <adam@>
2016-08-24 13:42:32 -0700

    add conditional statements to make player 2 ai

--- js/scripts.js
+++ js/scripts.js
@@ -7,34 +7,75 @@ var pigDice = function () {
 var player = function () {
   this.points = 0;
   this.diceData = 0;
+  this.ai = false;
 }
 
 player.prototype.adder = function (dice) {
   this.diceData += dice;
 }
 
-pigDice.prototype.game = function (currentPlayer) {
-  var points = 0;
-  var dice = Math.floor((Math.random() * 6) + 1);
-  if (dice === 1) {
-    currentPlayer.diceData = 0;
-    at++;
-    number++;
+player.prototype.aiHolder = function () {
+  if (this.diceData >= 6) {
+    this.points += this.diceData;
+    players[at].diceData = 0;
 
-    if (at >= players.length) {
-      at = 0;
-      number = 1;
+    if (this.points >= 20) {
+      $("#win").text("You win Player " + number);
+      $("#hold").hide();
+      $('#roll').hide();
+      $("#currentPoint").hide();
+      $("#currentPlayer").hide();
+      $("#Point").hide();
+      return true;
+    } else {
+      number++;
+      at++;
+
+      if (at >= players.length) {
+        at = 0;
+        number = 1;
+      }
+      displays();
+
+      return false;
     }
   } else {
-    currentPlayer.diceData += dice;
+    return true;
   }
+};
 
-  displays();
+pigDice.prototype.game = function (currentPlayer) {
 
-  if (currentPoint.points >= 100) {
-    $("#win").text("you win");
+  ai = true;
+
+  while (ai) {
+    var points = 0;
+    var dice = Math.floor((Math.random() * 6) + 1);
+    if (dice === 1) {
+      currentPlayer.diceData = 0;
+      at++;
+      number++;
+
+      ai = false;
+
+      if (at >= players.length) {
+        at = 0;
+        number = 1;
+      }
+    } else {
+      currentPlayer.diceData += dice;
+    }
+
+    if (currentPoint.points >= 100) {
+      $("#win").text("you win");
+    } else if (currentPlayer.ai && ai && !(currentPoint.points >= 100)) {
+      console.log("AI Dice: " + currentPlayer.diceData);
+      ai = currentPlayer.aiHolder();
+    } else {
+      ai = false;
+      displays();
+    }
   }
-
 }
 
 var game = new pigDice();
@@ -49,7 +90,7 @@ var displays = function () {
   $("#currentPoint").text("Current Point: " + players[at].points);
   $("#currentPlayer").text("Player " + number);
   $("#Point").text("Point: " + players[at].diceData);
-
+  console.log(players);
 }
 
 // User Interface Logic
@@ -62,6 +103,7 @@ $(document).ready(function() {
 
     var player1 = new player;
     var player2 = new player;
+    player2.ai = true;
 
     $("#currentPlayer").text("Player " + number);
     players.push(player1);
@@ -83,15 +125,17 @@ $(document).ready(function() {
       $("#Point").hide();
     } else {
       players[at].diceData = 0;
-
       number++;
       at++;
-
       if (at >= players.length) {
         at = 0;
         number = 1;
       }
       displays();
+
+      if (players[at].ai) {
+        game.game(players[at]);
+      }
     }
   });
 });
