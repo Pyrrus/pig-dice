6619984d9f0ebfd52d1b32ba888a2f09c098f65c
Adam Gorbahn and Martin Cartledge <martin@>
2016-08-24 09:01:53 -0700

    add business logic for player, holder, and current player. Add titles and start button to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
 // Business Logic
+var pigDice = function () {
+
+}
+
+// user input
+var player = function (ai) {
+  this.points = 0;
+  this.ai = ai;
+}
+
+var player.phototype.holder = function () {
+  
+}
 
 
+var pigDice.phototype.game = function (currentPlayer) {
+  var turnEnd = true;
+  var points = 0;
+  while (turnEnd) {
+    if (dice === 1) {
+      turnEnd = false;
+      points = 0;
+    } else {
+      points += dice;
+      turnEnd = holder();
+    }
+  }
 
+  currentPlayer.points += points;
 
-
-
+  if (currentPoint >= 100) {
+    you win
+  } else {
+    pass to next person
+  }
+}
 
 // User Interface Logic
